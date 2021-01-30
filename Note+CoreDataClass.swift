9faee5b0a11bee23f7b06d667102427ab3b2cef8
Jigar <Jigarpachchigar5561@gmail.com>
2021-01-29 22:42:45 -0500

    Notes CoreData Files

---
+++ Note+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Note+CoreDataClass.swift
+//  note_apollo_iOS
+//
+//  Created by Jigar Pachchigar on 29/01/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+
+}
